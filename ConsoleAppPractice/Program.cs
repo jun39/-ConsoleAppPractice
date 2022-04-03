5f9d3d64df6bd5aae4a47df2f570e866529c7a42
jun39 <bbbbb12312339@gmail.com>
2022-04-03 17:01:06 +0900

    added Book.cs

--- ConsoleAppPractice/Program.cs
+++ ConsoleAppPractice/Program.cs
@@ -6,13 +6,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppPractice
 {
+
+    //metaverse系の投稿をredditから取得していろいろやるアプリを作る
     internal class Program
     {
         static void Main(string[] args)
         {
             Random dice = new Random();
-            int roll = dice.Next(1, 7);
-            Console.Write(roll);
+            int roll = dice.Next(1, 10);
+            
+
+            Book book1 = new Book();
+            book1.title = "harry";
+            book1.author = "jke";
+            book1.pages = roll;
+            Console.WriteLine(book1.pages);
+
+
+
         }
     }
+
 }
