5fcd0cfecb31dfea16d24370a01238567acbdbae
jun39 <bbbbb12312339@gmail.com>
2022-04-04 18:13:57 +0900

    added Reddit.cs

---
+++ ConsoleAppPractice/Reddit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppPractice
+{
+    internal class Reddit
+    {
+        public string url;
+
+        //try catchでクエリーが文字列ではないときの例外を書く
+
+
+        public string GetComment(string query,string duration)
+        {
+
+        }
+    }
+}
