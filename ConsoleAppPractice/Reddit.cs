c0966fc46e90869fb1dd0d24a3830a95f0f9de50
jun39 <bbbbb12312339@gmail.com>
2022-04-04 18:19:02 +0900

    added constructor in Reddit class

--- ConsoleAppPractice/Reddit.cs
+++ ConsoleAppPractice/Reddit.cs
@@ -13,6 +13,11 @@ namespace ConsoleAppPractice
         //try catchでクエリーが文字列ではないときの例外を書く
 
 
+        public Reddit()
+        {
+            url = "https://api.pushshift.io/reddit/search/comment/?q=";
+        }
+
         public string GetComment(string query,string duration)
         {
 
